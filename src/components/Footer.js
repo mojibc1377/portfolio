ce640fe72574492c3203831b25e287ac5a1df631
Mojtaba Bagheri Chenari <mojibc1377@gmail.com>
2024-02-07 19:49:00 +0330

    Updated Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -23,7 +23,7 @@ function Footer() {
           <ul className="footer-icons">
             <li className="social-icons">
               <a
-                  href="https://https://github.com/mojibc1377"
+                  href="https://github.com/mojibc1377"
                 style={{ color: "white" }}
                 target="_blank" 
                 rel="noopener noreferrer"
